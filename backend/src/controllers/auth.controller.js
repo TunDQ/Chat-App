bc5d2c1e866c230032af761c0654ee7396673a90
AnhTuanVip <tuandqhe176834@fpt.edu.vn>
2025-04-05 04:26:48 +0700

    logic sign up

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,10 +1,16 @@
-import User from "../models/User.js";
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-exports.SignUp = async (req, res) => {
+
+export const SignUp = async (req, res) => {
   try {
     const { email, name, password } = req.body;
     if (!email || !name || !password) {
       return res.status(400).json("Please fill all the fields");
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json("Email already exists");
     }
     if (password.length < 6) {
       return res
@@ -13,13 +19,22 @@ exports.SignUp = async (req, res) => {
     }
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-    const user = User.findOne({ email });
-    if (user) {
-      return res.status(409).json("User already exists");
-    }
+
     const newUser = new User({ email, name, password: hashedPassword });
-    await newUser.save();
-    return res.status(201).json("User created successfully");
+    if (newUser) {
+      generateToken(newUser._id, res);
+
+      await newUser.save();
+
+      return res.status(201).json({
+        _id: newUser._id,
+        email: newUser.email,
+        name: newUser.name,
+        profileImage: newUser.profileImage,
+      });
+    } else {
+      return res.status(500).json({ message: "Invalid user data" });
+    }
   } catch (error) {
     return res.status(500).json(error.message);
   }
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
     name: { type: String, required: true },
-    phone: { type: String, required: true },
+    phone: { type: String },
     password: { type: String, required: true },
     profileImage: { type: String, default: "" },
   },
